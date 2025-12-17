b16090d6e0bc6132b68bba2fa2a23dfe8d09fa3b
unknown <sanchitkumarsingh098@gmail.com>
2025-12-17 15:15:29 +0530

    [sanchit kumar] added : FeetToMileYard

---
+++ gcr-codebase/programming-elements/level-2-problems/FeetToMileYard.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class FeetToMileYard {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+
+        System.out.print("Enter distance in feet: ");
+        double distanceInFeet = sc.nextDouble();
+
+
+        double distanceInYards = distanceInFeet / 3;
+        double distanceInMiles = distanceInYards / 1760;
+
+
+        System.out.println("Distance in Yards: " + distanceInYards);
+        System.out.println("Distance in Miles: " + distanceInMiles);
+
+        sc.close();
+    }
+}
