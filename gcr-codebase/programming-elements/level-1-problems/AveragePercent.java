3ee567e21476dfc1268708e8d26a2e7a607685b3
unknown <sanchitkumarsingh098@gmail.com>
2025-12-17 12:51:27 +0530

    [sanchit kumar] added:HeightConverter

---
+++ gcr-codebase/programming-elements/level-1-problems/AveragePercent.java
@@ -0,0 +1,19 @@
+import java.util.*;
+public class  AveragePercent{
+
+public static void main(String[] agrs){
+//Taking input ==>
+ Scanner sc =new Scanner(System.in);
+    int Physics =95;
+    int Chemistry = 96;
+    int Maths = 94;
+
+      double average = (Maths + Physics + Chemistry) / 3.0;
+
+     System.out.println("Sam’s average mark in PCM is " + average + "%");
+
+  
+
+}
+
+}
