f094397c9b97bd5284f4c80dcebbccec23e8374a
unknown <sanchitkumarsingh098@gmail.com>
2025-12-17 13:24:13 +0530

    [sanchit kumar] added:Kilometertomile

---
+++ gcr-codebase/programming-elements/level-1-problems/KilometersToMiles.java
@@ -0,0 +1,20 @@
+import java.util.*;
+public class  KilometersToMiles{
+
+public static void main(String[] agrs){
+//Taking input ==>
+ Scanner sc =new Scanner(System.in);
+    double kilometer =10.8;
+    double miles =1.6 ;
+   
+
+      double result = kilometer * miles;
+
+     System.out.println("The distance" +kilometer +"km in miles is"+ result);
+
+
+  
+
+}
+
+}
