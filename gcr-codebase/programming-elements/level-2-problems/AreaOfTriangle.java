2789efff3d88d9cdda0d2318544b8b058c4d23be
unknown <sanchitkumarsingh098@gmail.com>
2025-12-17 16:15:39 +0530

    [sanchit kumar] added : Areaof traingle code

--- gcr-codebase/programming-elements/level-2-problems/AreaOfTriangle.java
+++ gcr-codebase/programming-elements/level-2-problems/AreaOfTriangle.java
@@ -1,20 +1,20 @@
 import java.util.Scanner;
 
-public class PerimeterOfSquare{
+public class AreaofTriangle {
     public static void main(String[] args) {
+		
         Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter base number: ");
+        double base = scanner.nextDouble();
+        System.out.print("Enter height number: ");
+        double height = scanner.nextDouble();
+        double area= 0.5*height*base;
+        double baseIn = base / 2.54;
+        double heightIn = height / 2.54;
+        double areaIn = 0.5 * baseIn * heightIn;
 
-        System.out.print("Enter side : ");
-        double side = scanner.nextDouble();
-
-       
-        double perimeter = 4*side;
-
-     
-        System.out.println("The length of the side is"+ side +" whose perimeter is"+ perimeter+
-");
-
-   
+        System.out.println("The Area of the triangle in sq in is " + areaIn +
+                           " and sq cm is " + area);
+        sc.close();
     }
 }
-
