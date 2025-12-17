dbab48a328640967aa6c673f1d535663fcdf593e
unknown <sanchitkumarsingh098@gmail.com>
2025-12-17 13:20:10 +0530

    [sanchit kumar] added:DistanceInKilometer

---
+++ gcr-codebase/programming-elements/level-1-problems/DistanceInKilometer.java
@@ -0,0 +1,20 @@
+import java.util.*;
+public class   DistanceInKilometer{
+
+public static void main(String[] agrs){
+  
+
+Scanner input = new Scanner(System.in);
+
+double km = input.nextDouble();
+
+double miles = km * 0.621371;
+
+System.out.println("The total miles is " + miles + " mile for the given " + km + " km");
+
+       
+
+
+}
+
+}
