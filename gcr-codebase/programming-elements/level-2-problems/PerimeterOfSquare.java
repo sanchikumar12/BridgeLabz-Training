48556cbf3570e2e1d737da0023a3c474b8e5ddb8
unknown <sanchitkumarsingh098@gmail.com>
2025-12-17 15:17:26 +0530

    [sanchit kumar] added:PerimeterOfSquare code

---
+++ gcr-codebase/programming-elements/level-2-problems/PerimeterOfSquare.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class PerimeterOfSquare{
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter side : ");
+        double side = scanner.nextDouble();
+
+       
+        double perimeter = 4*side;
+
+     
+        System.out.println("The length of the side is"+ side +" whose perimeter is"+ perimeter);
+
+   
+    }
+}
+
