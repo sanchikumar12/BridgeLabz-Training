2e9853b05feed58c6d8a321949ae33d885eeba19
unknown <sanchitkumarsingh098@gmail.com>
2025-12-17 13:09:42 +0530

    [sanchit kumar] added : CalculateAge.java

---
+++ gcr-codebase/programming-elements/level-1-problems/CalculateAge.java
@@ -0,0 +1,16 @@
+import java.util.*;
+public class CalculateAge{
+
+public static void main(String[] agrs){
+ Scanner sc =new Scanner(System.in);
+    int birthYear =2000;
+    int currentYear = 2024;
+
+    int result = currentYear-birthYear;
+    System.out.println(result);
+
+  
+
+}
+
+}
