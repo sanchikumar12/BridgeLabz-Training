e66fa4f7feb0213ee554300e3c2f4c4c0e29aecf
unknown <sanchitkumarsingh098@gmail.com>
2025-12-18 15:48:55 +0530

    [sanchit kuamr] added:Countdown with for loop

---
+++ gcr-codebase/control-flow/level-1-problems/CountdownWithForLoop.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class CountdownWithForLoop {
+    public static void main(String[] args) {
+        
+
+        // Countdown using while loop
+        for (int a=8;a>0;a--) {
+            System.out.println(a);
+            a--;
+        }
+        
+
+
+        System.out.println("Launch!");
+     
+    }
+}
