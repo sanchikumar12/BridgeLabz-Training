31ce745c78448953db89ba7e13f4c2a84890b266
unknown <sanchitkumarsingh098@gmail.com>
2025-12-19 08:30:38 +0530

    [sanchit kumar] added:FizzBuzz2

---
+++ gcr-codebase/control-flow/level-2-problems/FizzBuzz2.java
@@ -0,0 +1,41 @@
+// Program FizzBuzz, take a number as user input, and check for a positive integer
+
+import java.util.Scanner;
+public class FizzBuzz2{
+	public static void main(String[] args){
+		int number;
+	
+		// Create a Scanner Object
+        Scanner input = new Scanner(System.in);
+		// Take number as input
+		System.out.println("Enter the number : ");
+		number = input.nextInt();
+		
+		// Checking if the number is Positive integer
+		if ( number >= 1 ){
+			// Using loop to display th number
+			int i = 1;
+			while ( i <= number ){
+				if( i % 3 == 0 && i % 5 != 0) {
+					System.out.println("Fizz");
+				} else if ( i % 3 != 0 && i % 5 == 0) {
+					System.out.println("Buzz");
+				} else if ( i % 3 == 0 && i % 5 == 0) {
+					System.out.println("FizzBuzz");
+				} else {
+					System.out.println( i );
+				}
+				i++;
+			}
+		} else {
+			System.out.println("Not a positive Integer");
+		}
+		
+		input.close();
+		
+	}
+}
+		
+				
+			
+		
