df965178d824989d3bb368af7da327d71b6ef8f6
unknown <sanchitkumarsingh098@gmail.com>
2025-12-19 08:34:25 +0530

    [sanchit kumar ]added : CountdownWithWhileLoop.java

---
+++ gcr-codebase/control-flow/level-1-problems/CountdownWithWhileLoop.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class CountdownWithForLoop {
+    public static void main(String[] args) {
+        
+
+        // Countdown using while loop
+        int a=8;
+        while (a>0) {
+            System.out.println(a);
+            a--;
+        }
+        
+
+
+        System.out.println("Launch!");
+     
+    }
+}
