5c72ddf364b0cd814ff9632caf4a4d021ebad259
unknown <sanchitkumarsingh098@gmail.com>
2025-12-22 00:00:12 +0530

    [sanchit kumar] :added NumberCheckerTwo code

---
+++ gcr-codebase/Methods/level-3-problems/NumberCheckerTwo.java
@@ -0,0 +1,100 @@
+import java.util.Scanner;
+
+public class NumberCheckerTwo {
+
+    // Method to find count of digits
+    public static int countDigits(int number) {
+        if (number == 0) {
+            return 1;
+        }
+        int count = 0;
+        while (number != 0) {
+            count++;
+            number /= 10;
+        }
+        return count;
+    }
+
+    // Method to store digits of the number in an array
+    public static int[] storeDigits(int number) {
+        int count = countDigits(number);
+        int[] digits = new int[count];
+        for (int i = count - 1; i >= 0; i--) {
+            digits[i] = number % 10;
+            number /= 10;
+        }
+        return digits;
+    }
+
+    // Method to find sum of digits using digits array
+    public static int sumOfDigits(int[] digits) {
+        int sum = 0;
+        for (int digit : digits) {
+            sum += digit;
+        }
+        return sum;
+    }
+
+    // Method to find sum of squares of digits using Math.pow()
+    public static double sumOfSquaresOfDigits(int[] digits) {
+        double sum = 0;
+        for (int digit : digits) {
+            sum += Math.pow(digit, 2);
+        }
+        return sum;
+    }
+
+    // Method to check Harshad number
+    public static boolean isHarshadNumber(int number, int[] digits) {
+        int sum = sumOfDigits(digits);
+        return sum != 0 && number % sum == 0;
+    }
+
+    // Method to find frequency of each digit
+    public static int[][] findDigitFrequency(int[] digits) {
+        int[][] frequency = new int[10][2];
+        for (int i = 0; i < 10; i++) {
+            frequency[i][0] = i;
+            frequency[i][1] = 0;
+        }
+        for (int digit : digits) {
+            frequency[digit][1]++;
+        }
+
+        return frequency;
+    }
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a number: ");
+        int number = sc.nextInt();
+        int count = countDigits(number);
+        System.out.println("Number of digits: " + count);
+
+        int[] digits = storeDigits(number);
+        System.out.print("Digits: ");
+        for (int d : digits) {
+            System.out.print(d + " ");
+        }
+        System.out.println();
+
+        int sumDigits = sumOfDigits(digits);
+        System.out.println("Sum of digits: " + sumDigits);
+
+        double sumSquares = sumOfSquaresOfDigits(digits);
+        System.out.println("Sum of squares of digits: " + sumSquares);
+
+        System.out.println("Is Harshad Number: " + isHarshadNumber(number, digits));
+
+        int[][] frequency = findDigitFrequency(digits);
+        System.out.println("Digit Frequency:");
+        System.out.println("Digit  Frequency");
+        for (int i = 0; i < frequency.length; i++) {
+            if (frequency[i][1] > 0) {
+                System.out.println(frequency[i][0] + "      " +frequency[i][1]);
+            }
+        }
+    }
+}
