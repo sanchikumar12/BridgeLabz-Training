e267e5300ca77a7489d07d0e80cafd3d54cacbf9
unknown <sanchitkumarsingh098@gmail.com>
2025-12-21 20:03:25 +0530

    [sanchit kumar] :added Quadratic code

---
+++ gcr-codebase/Methods/level-2-problems/Quadratic.java
@@ -0,0 +1,48 @@
+import java.util.Scanner;
+
+public class Quadratic {
+
+    public double[] findRoots(double a, double b, double c) {
+        double delta = Math.pow(b, 2) - 4 * a * c;
+
+        if (delta > 0) {
+            double root1 = (-b + Math.sqrt(delta)) / (2 * a);
+            double root2 = (-b - Math.sqrt(delta)) / (2 * a);
+            return new double[]{root1, root2};
+        } else if (delta == 0) {
+            double root = -b / (2 * a);
+            return new double[]{root};
+        } else {
+            return new double[]{}; 
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        Quadratic quad = new Quadratic();
+
+        System.out.print("Enter value of a: ");
+        double a = sc.nextDouble();
+
+        System.out.print("Enter value of b: ");
+        double b = sc.nextDouble();
+
+        System.out.print("Enter value of c: ");
+        double c = sc.nextDouble();
+
+        double[] roots = quad.findRoots(a, b, c);
+
+        if (roots.length == 2) {
+            System.out.println("Two Real Roots:");
+            System.out.println("Root 1: " + roots[0]);
+            System.out.println("Root 2: " + roots[1]);
+        } else if (roots.length == 1) {
+            System.out.println("One Real Root:");
+            System.out.println("Root: " + roots[0]);
+        } else {
+            System.out.println("No Real Roots (Delta is Negative)");
+        }
+
+        sc.close();
+    }
+}
