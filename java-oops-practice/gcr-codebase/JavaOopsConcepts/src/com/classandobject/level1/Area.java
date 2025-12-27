2598e3f66543bdc3e27f07643efc06a574cf5390
unknown <sanchitkumarsingh098@gmail.com>
2025-12-27 12:12:38 +0530

    [sanchit kumar] added:Area code

---
+++ java-oops-practice/gcr-codebase/JavaOopsConcepts/src/com/classandobject/level1/Area.java
@@ -0,0 +1,29 @@
+package com.classandobject.level1;
+
+public class Area {
+   double radius;
+   
+   public Area(double radius) {
+	   this.radius=radius;
+   }
+   
+ //Creating method for calculating area of circle
+ 	public void areaCircle() {
+ 		System.out.printf("Area of circle: %.4f",(Math.PI*radius*radius));
+ 	}
+ 	
+ 	//Creating method for calculating circumference of circle
+ 		public void circumferenceCircle() {
+ 			System.out.printf("\nCircumference of circle: %.4f ",(2*Math.PI*radius));
+ 		}
+
+ 	
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Area circle = new Area(2.5);
+		//constructor invoke
+        circle.circumferenceCircle();
+	}
+
+}
