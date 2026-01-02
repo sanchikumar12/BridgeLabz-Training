8e3666036b845105079dfb8ed43c13983347f4c8
unknown <sanchitkumarsingh098@gmail.com>
2026-01-02 22:05:38 +0530

    [sanchit kumar] :added AnimalHierarchy code

---
+++ java-oops-practice/gcr-codebase/JavaOopsConcepts/src/com/inheritance/levelone/AnimalHierarchy.java
@@ -0,0 +1,35 @@
+package com.inheritance.levelone;
+
+public class AnimalHierarchy {
+	
+
+ 
+public void displayName() {
+	
+}
+
+public void displayAge() {
+	
+}
+	
+
+	public static void main(String[] args) {
+		
+		Bird bird =new Bird("Owl",34);
+		bird.displayName();
+		bird.displayAge();
+		
+		Cat cat=new Cat("Nora",10);
+		cat.displayName();
+		cat.displayAge();
+		
+		Dog dog=new Dog("Labra",12);
+		dog.displayName();
+		dog.displayAge();
+		
+		
+		
+     
+	}
+
+}
