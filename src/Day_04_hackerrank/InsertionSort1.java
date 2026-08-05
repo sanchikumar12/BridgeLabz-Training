dadfbdec7aa63b5579e7f4b4e6684ff458d6825d
unknown <sanchitkumarsingh098@gmail.com>
2026-08-05 21:42:54 +0530

    day 4  task

---
+++ src/Day_04_hackerrank/InsertionSort1.java
@@ -0,0 +1,14 @@
+package Day_04_hackerrank;
+
+/**
+ * Q1. Insertion Sort - Part 1 [Easy]
+ * You are given a sorted array with one unsorted number at the rightmost position. Write code that inserts
+ * this number into its correct position, printing the array every time an element is shifted. This isolates a
+ * single 'insert' step of Insertion Sort, exactly as explained with the key/shift logic in class.
+ * Link: https://www.hackerrank.com/challenges/insertionsort1/problem
+ */
+public class InsertionSort1 {
+    public static void main(String[] args) {
+        // Implementation here
+    }
+}
